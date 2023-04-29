a5f8857a27bdfeea6bba658a808bf32be16f0041
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-04-28 23:04:54 -0300

    dto conceitos

---
+++ lib/layers/data/dto/carro_dto.dart
@@ -0,0 +1,34 @@
+// DTO Data Transfer Object
+import 'package:cleandart/layers/domain/entities/carro_entity.dart';
+
+class CarroDto extends CarroEntity {
+  // entidade tem que ser modelada de acordo com a api
+  String placa;
+  int quantidadeDePortas;
+  double valorFinal;
+
+  CarroDto({
+    required this.placa,
+    required this.quantidadeDePortas,
+    required this.valorFinal,
+  }) : super(
+          valor: valorFinal,
+          qtdPortas: quantidadeDePortas,
+          placa: placa,
+        );
+
+  toMap() {
+    return {
+      'placa': this.placa,
+      'quantidadeDePortas': this.quantidadeDePortas,
+      'valorFinal': this.valorFinal,
+    };
+  }
+
+  CarroDto fromMap(Map map) {
+    return CarroDto(
+        placa: map['placa'],
+        quantidadeDePortas: map['quantidadeDePortas'],
+        valorFinal: map['valorFinal']);
+  }
+}
